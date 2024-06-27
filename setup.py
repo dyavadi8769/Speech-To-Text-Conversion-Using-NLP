086ea9a43511d23050d1b72e90560aa476bede32
dyavadi8769 <sdyavadi@tamu.edu>
2024-06-27 17:57:33 -0500

    First commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="Speech To Text",
+    version="0.0.1",
+    author="Sai Kiran Reddy Dyavadi",
+    author_email="dyavadi324@gmail.com",
+    packages=find_packages(),
+    install_requires=[]
+)
---
+++ test.py
@@ -0,0 +1,6 @@
+from STT.exceptions import STTException
+import sys
+
+from STT.logger import logging
+
+logging.info("this is my code")

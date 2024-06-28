edc49b63919b0955079eaf1e6c227874685e490b
dyavadi8769 <sdyavadi@tamu.edu>
2024-06-28 15:47:08 -0500

    Data Preprocessing

--- setup.py
+++ setup.py
@@ -1,9 +1,9 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="Speech To Text",
+    name="STT",
     version="0.0.1",
-    author="Sai Kiran Reddy Dyavadi",
+    author="Sai Kiran Reddy",
     author_email="dyavadi324@gmail.com",
     packages=find_packages(),
     install_requires=[]
--- test.py
+++ test.py
@@ -1,5 +1,9 @@
-from STT.entity.config_entity import DataIngestionConfig
+from STT.entity.config_entity import DataIngestionConfig, DataPreprocessingConfig
 from STT.components.data_ingestion import DataIngestion
+from STT.components.data_preprocessing import DataPreprocessing
+
 
 di_ins=DataIngestion(DataIngestionConfig)
 di_art=di_ins.initiate_data_ingestion()
+dp_ins=DataPreprocessing(DataPreprocessingConfig, di_art)
+dp_art=dp_ins.initiate_data_preprocessing()
